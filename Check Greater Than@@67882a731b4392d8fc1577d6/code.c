f27862f3d4c11b130530458e4c2206bdf4eefb21
thegarvitsingla <107167559+thegarvitsingla@users.noreply.github.com>
2025-02-07 16:46:20 +0530

    Solution to Check Greater Than

--- Check Greater Than@@67882a731b4392d8fc1577d6/code.c
+++ Check Greater Than@@67882a731b4392d8fc1577d6/code.c
@@ -5,8 +5,8 @@ int main() {
     int first;
     int second;
     scanf("%i %i",&first,&second);
-    printf("%i and %i",first,second);
-    if (first>>second){
+    
+    if (first>second){
         printf("True");
     }else{
         printf("False");
