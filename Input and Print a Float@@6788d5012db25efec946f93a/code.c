74924fd7c63a8d69cbcefef6773fefecfed08212
thegarvitsingla <107167559+thegarvitsingla@users.noreply.github.com>
2025-03-09 22:15:55 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -1,8 +1,21 @@
+#include <dirent.h>
 #include <stdio.h>
 
+int main(void) {
+    // Open the current directory
+    DIR *dir;
+    struct dirent *ent;
 
-int main() {
-   float i;
-   scanf("%f",&i);
-   printf("You entered: %.2f",i);
+    if ((dir = opendir(".")) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            // Print the file/directory name
+            printf("%s\n", ent->d_name);
+        }
+        closedir(dir);
+    } else {
+        // Handle error if directory cannot be opened
+        perror("Could not open directory");
+    }
+
+    return 0;
 }
