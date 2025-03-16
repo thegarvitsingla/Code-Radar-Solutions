2976158b6b665dbc8351178f7724c10ffe1ada47
thegarvitsingla <107167559+thegarvitsingla@users.noreply.github.com>
2025-03-16 11:22:53 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-#include <dirent.h>
-int listFiles;
-int bubbleSort(const char *path) {
-    struct dirent *entry;
-    DIR *directory = opendir(path);
-
-    if (directory == NULL) {
-        printf("Could not open directory\n");
-        return 1;
-    }
-
-    while ((entry = readdir(directory)) != NULL) {
-        printf("%s\n", entry->d_name);
-    }
-
-    closedir(directory);
-}
+#include <stdlib.h>
 
 int main() {
-    listFiles("."); // "." refers to the current directory
+    extern char **environ; 
+    char **env = environ;
+
+    while (*env) {
+        printf("%s\n", *env);
+        env++;
+    }
+
     return 0;
 }
