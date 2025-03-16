101631033d2ad1910f73a083f48a0e711533a36c
thegarvitsingla <107167559+thegarvitsingla@users.noreply.github.com>
2025-03-16 11:27:10 +0530

    Solution to Input and Print a Character

--- Input and Print a Character@@6788d5012db25efec946f93b/code.c
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-
-
+hashtag#include <dirent.h>
 
 int main() {
-   char i;
-   scanf("%c",&i);
-   printf("You entered: %c",i);
+ struct dirent *entry;
+ DIR *directory = opendir(".."); 
+
+ if (directory == NULL) {
+ printf("Could not open parent directory\n");
+ return 1;
+ }
+
+ printf("Files in the parent directory:\n");
+ while ((entry = readdir(directory)) != NULL) {
+ printf("%s\n", entry->d_name);
+ }
+
+ closedir(directory);
+ return 0;
 }
